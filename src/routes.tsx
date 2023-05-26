3fef73e60918d604e528045cea25ed13c95f675c
jtwjs <xodnd9503@gmail.com>
2023-05-26 15:02:17 +0900

    :sparkles: #9 implements routes

---
+++ src/routes.tsx
@@ -0,0 +1,16 @@
+import { Layout } from "@/components/layout";
+import PATH from "@/constants/path";
+import * as Page from "@/pages";
+
+const routes = [
+  {
+    element: <Layout />,
+    children: [
+      { path: PATH.ROOT, element: <Page.Home /> },
+      { path: PATH.PRODUCTS, element: <Page.Products /> },
+      { path: `${PATH.PRODUCTS}/:id`, element: <Page.ProductDetail /> },
+    ],
+  },
+];
+
+export default routes;
