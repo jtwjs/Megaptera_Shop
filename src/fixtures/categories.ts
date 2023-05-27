d1d5613dde095d583ea3d8a9bd52ad13cb004563
jtwjs <xodnd9503@gmail.com>
2023-05-27 12:33:18 +0900

    :sparkles: add category fixtures and mock fetch categories API with msw

---
+++ src/fixtures/categories.ts
@@ -0,0 +1,6 @@
+import type { Category } from "@/types/categories";
+
+export const categories: Category[] = [
+  { id: "category-01", name: "Category #1" },
+  { id: "category-02", name: "Category #2" },
+];
