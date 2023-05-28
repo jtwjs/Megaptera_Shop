1a7305cd794bbf97ee80ce2f533b67a011c913ad
jtwjs <xodnd9503@gmail.com>
2023-05-28 12:43:50 +0900

    :sparkles: #11 add product fixtures and mock fetch products API with msw

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,25 @@
 import { rest } from "msw";
 
-import { categories } from "@/fixtures/categories";
+import * as fixtures from "@/fixtures";
 
 const handlers = [
-  rest.get("/categories", (req, res, ctx) => res(ctx.json({ categories }))),
+  rest.get("/categories", (req, res, ctx) =>
+    res(ctx.json({ categories: fixtures.categories }))
+  ),
+  rest.get("/products", (req, res, ctx) => {
+    const categoryId = req.url.searchParams.get("categoryId");
+
+    if (categoryId) {
+      return res(
+        ctx.json({
+          products: fixtures.products.filter(
+            (product) => product.category.id === (categoryId as string)
+          ),
+        })
+      );
+    }
+    return res(ctx.json({ products: fixtures.products }));
+  }),
 ];
 
 export default handlers;
