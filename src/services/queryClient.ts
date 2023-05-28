4c149bb1ec1c5aadc4b833a426883abd26963690
jtwjs <xodnd9503@gmail.com>
2023-05-28 12:44:29 +0900

    :sparkles: #11 add fetch products api handler and useProducts query hooks

--- src/services/queryClient.ts
+++ src/services/queryClient.ts
@@ -1,15 +1,4 @@
 import { QueryClient } from "@tanstack/react-query";
-import axios from "axios";
-
-function queryErrorHandler(error: unknown): void {
-  console.error(error);
-  const message = axios.isAxiosError(error)
-    ? error.response?.data.message
-    : "error connecting to server";
-  if (typeof window !== undefined) {
-    alert(message);
-  }
-}
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,11 +6,8 @@ const queryClient = new QueryClient({
       staleTime: 1000 * 60 * 10,
       cacheTime: 1000 * 60 * 15,
       retry: 0,
-      onError: (err) => queryErrorHandler(err),
     },
-    mutations: {
-      onError: (err) => queryErrorHandler(err),
-    },
+    mutations: {},
   },
 });
 
