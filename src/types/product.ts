bf0f93d53cfef503dd1ed617fb0cb281c7898343
jtwjs <xodnd9503@gmail.com>
2023-05-28 12:42:10 +0900

    :label: #11 add product type

---
+++ src/types/product.ts
@@ -0,0 +1,42 @@
+import type { Category } from "./categories";
+
+export type Image = {
+  url: string;
+};
+
+export interface Product {
+  id: string;
+  category: Category;
+  thumbnail: Image;
+  name: string;
+  price: number;
+}
+
+export interface ProductApiRequest {
+  categoryId?: string;
+}
+
+export interface ProductsApiResponse {
+  products: Product[];
+}
+
+export type ProductOptionItem = {
+  id: string;
+  name: string;
+};
+
+export interface ProductOption {
+  id: string;
+  name: string;
+  items: ProductOptionItem[];
+}
+
+export interface ProductDetail {
+  id: string;
+  category: Category;
+  images: Image[];
+  name: string;
+  price: number;
+  options: ProductOption[];
+  description: string;
+}
