0df9c60dc31e4a38fdc5975c98cf24f97f7075bc
jtwjs <xodnd9503@gmail.com>
2023-05-29 16:08:22 +0900

    :sparkles: add number foramting function

---
+++ src/utils/format/format.test.ts
@@ -0,0 +1,13 @@
+import { numberFormat } from "./format";
+
+describe("format", () => {
+  describe("numberFormat", () => {
+    it("return a string with commas appended every 3 digits", () => {
+      expect(numberFormat(0)).toBe("0");
+      expect(numberFormat(30)).toBe("30");
+      expect(numberFormat(3_000)).toBe("3,000");
+      expect(numberFormat(30_000)).toBe("30,000");
+      expect(numberFormat(950_300)).toBe("950,300");
+    });
+  });
+});
