cd154171686aab9b7882cc3214b54616deb48d4e
jtwjs <xodnd9503@gmail.com>
2023-05-31 22:21:27 +0900

    :recycle: add disabled props & style

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -5,15 +5,17 @@ import * as S from "./Button.styled";
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   type?: "button" | "submit";
   label: string;
+  disabled?: boolean;
 }
 
 export default function Button({
   type = "button",
   label,
+  disabled = false,
   ...other
 }: ButtonProps) {
   return (
-    <S.Button type={type} {...other}>
+    <S.Button type={type} disabled={disabled} {...other}>
       {label}
     </S.Button>
   );
