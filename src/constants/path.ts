36bc6c74b7e51bd52099c754c703e620efbc98f5
jtwjs <xodnd9503@gmail.com>
2023-05-31 22:22:55 +0900

    :sparkles: #19 add login page

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -2,6 +2,9 @@ const PATH = {
   ROOT: "/",
   PRODUCTS: "/products",
   CART: "/cart",
+  LOGIN: "/login",
 };
+
+export const NON_SAVED_PATH = [PATH.LOGIN];
 
 export default PATH;
---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "@/components/login";
+
+export default function LoginPage() {
+  return (
+    <section>
+      <h1>로그인</h1>
+      <LoginForm />
+    </section>
+  );
+}
