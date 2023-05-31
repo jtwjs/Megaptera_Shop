f6d2f303e2e0ff96f35744ab89ab87486c8d95b7
jtwjs <xodnd9503@gmail.com>
2023-05-31 22:21:52 +0900

    :sparkles: add valid message constants

---
+++ src/constants/validMsg.ts
@@ -0,0 +1,10 @@
+const VALID_MSG = {
+  REQUIRED: "필수 입력 정보입니다",
+  EMAIL: "이메일 형식이 올바르지 않습니다.",
+  NAME: "이름 형식이 올바르지 않습니다.",
+  PASSWORD: "비밀번호 형식이 올바르지 않습니다.",
+  PASSWORD_C: "비밀번호가 일치하지 않습니다.",
+  LOGIN: "이메일 또는 비밀번호가 일치하지 않습니다.",
+};
+
+export default VALID_MSG;
