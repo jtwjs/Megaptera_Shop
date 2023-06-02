f05bc04d55dcff200c45c9d1a9dbaa61731345f9
jtwjs <xodnd9503@gmail.com>
2023-06-02 12:42:59 +0900

    :sparkles: #23 add orders, orderDetail page

---
+++ src/pages/order/detail/OrderDetailPage.tsx
@@ -0,0 +1,10 @@
+import { OrderDetail } from "@/components/orders";
+
+export default function OrderDetailPage() {
+  return (
+    <section>
+      <h1>주문 상세</h1>
+      <OrderDetail />
+    </section>
+  );
+}
