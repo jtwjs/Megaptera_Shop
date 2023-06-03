ed8a50ebde395f599c04979c2c9ab2f9f18dc6ec
jtwjs <xodnd9503@gmail.com>
2023-06-03 15:21:22 +0900

    :sparkles: #27 implement PaymentForm component

---
+++ src/components/payment/form/PaymentForm.styled.ts
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const PaymentForm = styled.div`
+  h3 {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
+
+  input {
+    width: 50rem;
+  }
+`;
+
+export const PostalCodeField = styled.div`
+  display: flex;
+
+  & > div {
+    margin-right: 1rem;
+  }
+`;
