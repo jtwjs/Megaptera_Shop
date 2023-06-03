f5321a4c8cf4bbc36f495842a9c33ac49c1de9ce
jtwjs <xodnd9503@gmail.com>
2023-06-03 16:48:50 +0900

    :recycle: #27 value to object

--- src/stores/paymentForm/PaymentFormStore.test.ts
+++ src/stores/paymentForm/PaymentFormStore.test.ts
@@ -14,7 +14,7 @@ describe("PaymentFormStore", () => {
       const name = "정태웅";
       store.changeName(name);
 
-      expect(store.name).toBe(name);
+      expect(store.receiver.name).toBe(name);
     });
   });
 
@@ -24,8 +24,8 @@ describe("PaymentFormStore", () => {
       const postalCode = "1550";
       store.changeAddress(address, postalCode);
 
-      expect(store.address).toBe(address);
-      expect(store.postalCode).toBe(postalCode);
+      expect(store.receiver.address1).toBe(address);
+      expect(store.receiver.postalCode).toBe(postalCode);
     });
   });
   context("with changeAddressDetail", () => {
@@ -33,7 +33,7 @@ describe("PaymentFormStore", () => {
       const detail = "501호";
       store.changeAddressDetail(detail);
 
-      expect(store.addressDetail).toBe(detail);
+      expect(store.receiver.address2).toBe(detail);
     });
   });
   context("with changePhoneNumber", () => {
@@ -41,15 +41,15 @@ describe("PaymentFormStore", () => {
       const phone = "01025919503";
       store.changePhoneNumber(phone);
 
-      expect(store.phoneNumber).toBe(phone);
+      expect(store.receiver.phoneNumber).toBe(phone);
     });
 
     it("only numeric characters are entered", () => {
       const phone = "010-2591-9503";
       store.changePhoneNumber(phone);
 
-      expect(store.phoneNumber).not.toBe(phone);
-      expect(store.phoneNumber).toBe("01025919503");
+      expect(store.receiver.phoneNumber).not.toBe(phone);
+      expect(store.receiver.phoneNumber).toBe("01025919503");
     });
   });
 });
