eef4a4c24d8b88f58dea20af72d9c11eb5bbc232
jtwjs <xodnd9503@gmail.com>
2023-06-03 15:21:48 +0900

    :sparkles: #27 implement PaymentView component

---
+++ src/components/payment/paymentView/PaymentView.styled.ts
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const PaymentView = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+`;
+
+export const TotalPrice = styled.dl`
+  display: flex;
+  column-gap: 4px;
+  font-size: 2rem;
+  font-weight: 600;
+`;
---
+++ src/components/payment/paymentView/PaymentView.tsx
@@ -0,0 +1,26 @@
+import { LineItemTable } from "@/components/common";
+import { useFetchCartList } from "@/services/useCart";
+import { numberFormat } from "@/utils/format";
+
+import PaymentForm from "../form/PaymentForm";
+
+import * as S from "./PaymentView.styled";
+
+export default function PaymentView() {
+  const { data } = useFetchCartList();
+
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <S.PaymentView>
+      <LineItemTable caption="결제 목록" lineItems={data.lineItems} />
+      <S.TotalPrice>
+        <dt>합계:</dt>
+        <dd>{numberFormat(data.totalPrice)}원</dd>
+      </S.TotalPrice>
+      <PaymentForm />
+    </S.PaymentView>
+  );
+}
