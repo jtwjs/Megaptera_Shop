4652981bf39f5719a7948c0da1b870f7c46c18c8
jtwjs <xodnd9503@gmail.com>
2023-06-03 15:25:08 +0900

    :label: add trigger type

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,3 +1,10 @@
 export type ApiHandler<Q, S = void> = (req?: Q) => Promise<S>;
 
 export type WithId<T> = { id: string } & T;
+
+export interface TriggerArgs {
+  isOpen: boolean;
+  handleToggle: (event: React.MouseEvent) => void;
+}
+
+export type Trigger = (args: TriggerArgs) => React.ReactElement;
