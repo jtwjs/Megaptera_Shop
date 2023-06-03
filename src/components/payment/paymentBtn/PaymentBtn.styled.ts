60b74a3034a8fd027c3f7e6948612527ccb362d5
jtwjs <xodnd9503@gmail.com>
2023-06-03 16:49:49 +0900

    :sparkles: #27 add payment button

---
+++ src/components/payment/paymentBtn/PaymentBtn.styled.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const PaymentBtn = styled.div`
+  p {
+    margin-block: 2rem;
+    color: ${(props) => props.theme.colors.primary};
+    font-size: 2rem;
+    font-weight: bold;
+  }
+`;
