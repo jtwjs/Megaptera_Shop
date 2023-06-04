69b9d7ee9465eb0b33c2a36b3d1038d692378917
jtwjs <xodnd9503@gmail.com>
2023-06-05 00:38:02 +0900

    :wrench: #27 setting cypress iframe

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   projectId: "efmgmm",
+  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
